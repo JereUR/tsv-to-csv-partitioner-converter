1b79cb3c9f2438720f1b86381c13dbaf79fc3ae5
JereUR <jeremias.jdv@gmail.com>
2023-11-04 21:06:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const Papa = require('papaparse')
+
+const filePath = './datasets-files/data.tsv'
+const chunkSize = 100 * 1024 * 1024 // 100 MB en bytes
+
+let readStream = fs.createReadStream(filePath, {
+  encoding: 'utf8',
+  highWaterMark: chunkSize
+})
+let chunkCount = 0
+
+readStream.on('data', (chunk) => {
+  chunkCount++
+  const chunkFilePath = `./results-chunks/chunk_${chunkCount}.tsv`
+
+  fs.writeFile(chunkFilePath, chunk, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error al escribir el archivo ${chunkFilePath}`, err)
+    } else {
+      console.log(`Archivo ${chunkFilePath} creado exitosamente.`)
+
+      // Convertir el chunk a CSV aquí, si lo deseas
+      Papa.parse(chunk, {
+        delimiter: '\t',
+        complete: (result) => {
+          const csvContent = Papa.unparse(result.data)
+          const csvFilePath = `./results-csv/chunk_${chunkCount}.csv`
+          fs.writeFileSync(csvFilePath, csvContent)
+          console.log(`Archivo ${csvFilePath} convertido exitosamente.`)
+        },
+        error: (err) => {
+          console.error(`Error al convertir ${chunkFilePath} a CSV:`, err)
+        }
+      })
+    }
+  })
+})
+
+readStream.on('end', () => {
+  console.log('Lectura del archivo completa.')
+})
